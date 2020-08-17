0de2ef7bff3ef489ed9463bcffe0031fbc614d21
John Hammer <johnwhammer@gmail.com>
2020-08-17 17:01:03 +0000

    generated new key and hid in environ

--- Pro_Profile/settings.py
+++ Pro_Profile/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%-pty&wg7vv7m74$^f8p3_-mqa+-5e+v+1fzm*sxdnpuy$&w!h'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
